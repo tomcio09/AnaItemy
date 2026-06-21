612165d003c89cc58d0cec06d3e64f06e3f77f56
Tomasz Gregor <127117177+tomcio09@users.noreply.github.com>
2026-06-21 20:38:28 +0200

    Update AnaItemy.java

--- src/main/java/pl/anaheim/anaitemy/AnaItemy.java
+++ src/main/java/pl/anaheim/anaitemy/AnaItemy.java
@@ -3,6 +3,7 @@ package pl.anaheim.anaitemy;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.anaheim.anaitemy.commands.ItemyEventoweCommand;
 import pl.anaheim.anaitemy.listeners.ExcaliburListener;
+import pl.anaheim.anaitemy.listeners.GUIListener;
 import pl.anaheim.anaitemy.listeners.TotemListener;
 
 public class AnaItemy extends JavaPlugin {
@@ -19,6 +20,7 @@ public class AnaItemy extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new TotemListener(this), this);
         getServer().getPluginManager().registerEvents(new ExcaliburListener(this), this);
+        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
 
         getLogger().info("AnaItemy zostal wlaczony!");
     }
