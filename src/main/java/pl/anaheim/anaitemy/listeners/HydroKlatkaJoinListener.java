2ebaf72c9973c94b98b6398b1932f7415c1d3736
Tomasz Gregor <127117177+tomcio09@users.noreply.github.com>
2026-06-22 12:21:23 +0200

    Update HydroKlatkaJoinListener.java

--- src/main/java/pl/anaheim/anaitemy/listeners/HydroKlatkaJoinListener.java
+++ src/main/java/pl/anaheim/anaitemy/listeners/HydroKlatkaJoinListener.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import pl.anaheim.anaitemy.AnaItemy;
 import pl.anaheim.anaitemy.managers.HydroKlatkaManager;
 import pl.anaheim.anaitemy.models.ActiveHydroKlatka;
@@ -24,18 +25,31 @@ public class HydroKlatkaJoinListener implements Listener {
 
         // Sprawdź czy gracz był w klatce gdy wyszedł z serwera
         ActiveHydroKlatka klatka = manager.getKlatkaForPlayer(player);
-        if (klatka == null) return;
+        if (klatka != null) {
+            Location playerLoc = player.getLocation();
+            Location center = klatka.getCenter();
 
-        // Klatka nadal istnieje - sprawdź czy gracz jest w środku
-        Location playerLoc = player.getLocation();
-        Location center = klatka.getCenter();
-
-        // Jeśli gracz jest poza klatką - teleportuj do centrum
-        if (playerLoc.distance(center) > klatka.getRadius()) {
-            Location teleportLoc = center.clone();
-            teleportLoc.setYaw(playerLoc.getYaw());
-            teleportLoc.setPitch(playerLoc.getPitch());
-            player.teleport(teleportLoc);
+            // Jeśli gracz jest poza klatką - teleportuj do centrum
+            if (playerLoc.distance(center) > klatka.getRadius()) {
+                Location teleportLoc = center.clone();
+                teleportLoc.setYaw(playerLoc.getYaw());
+                teleportLoc.setPitch(playerLoc.getPitch());
+                player.teleport(teleportLoc);
+            }
         }
+
+        // Sprawdź czy gracz ma cooldown i wznów action bar
+        if (manager.isPlayerOnCooldown(player)) {
+            manager.startCooldownDisplay(player);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        HydroKlatkaManager manager = plugin.getHydroKlatkaManager();
+
+        // Zatrzymaj action bar display przy wylogowaniu
+        manager.stopCooldownDisplay(player);
     }
 }
