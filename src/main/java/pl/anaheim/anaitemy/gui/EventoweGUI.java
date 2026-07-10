3d0e3a540a382dc71884f51faa447feb9bfd5434
Tomasz Gregor <127117177+tomcio09@users.noreply.github.com>
2026-07-10 12:01:03 +0200

    Update EventoweGUI.java

--- src/main/java/pl/anaheim/anaitemy/gui/EventoweGUI.java
+++ src/main/java/pl/anaheim/anaitemy/gui/EventoweGUI.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
+import pl.anaheim.anaitemy.items.AnarchicznySetItem;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
